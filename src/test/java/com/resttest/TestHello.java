76ce761e110ef03a1e66372235e450fb32e34239
Rebeca Teodoro <rebeca.teodoro@gmail.com>
2020-01-08 10:02:59 -0300

    Initial setup for the tests

--- src/test/java/com/resttest/TestHello.java
+++ src/test/java/com/resttest/TestHello.java
@@ -1,19 +1,19 @@
 package com.resttest;
 
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("test")
-class ResttestApplicationTests {
-
+public class TestHello {
+	
 	@Test
-	void contextLoads() {
+	public void testHello() {
+		assertEquals(1, 1);
 	}
 
 }
