c0ef2685e5a0bdf9d14d5ea5ad931079b2ee0bb8
Rebeca Teodoro <rebeca.teodoro@gmail.com>
2020-01-30 15:57:17 -0300

    Creation of a new docker image and attempt to include Cloud-Eureka

--- src/main/java/com/resttest/ResttestApplication.java
+++ src/main/java/com/resttest/ResttestApplication.java
@@ -3,6 +3,7 @@ package com.resttest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ResttestApplication {
 
